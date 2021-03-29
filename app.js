864474b628b5cafd378232a2c62d938d0edfb82d
imjp19 <jaypatel19124@gmail.com>
2021-03-29 23:05:19 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,96 @@
+"use strict";
+const niceInvoice = require('./nice-invoice/app.js');
+const nodemailer = require('nodemailer');
+const invoiceDetail = {
+  niceInvoice: {
+
+    prakarsh_logo: "prakarsh1.png"
+  },
+  shipping: {
+    name: "Jay Patel",
+    college: "Sardar Vallabhbhai Patel Institute of Technology, Vasad",
+    mobile: "+91-9879726851",
+    email: "jaypatel19124@gmail.com",
+  },
+  items: [
+    {
+      item: "Chair",
+      price: 50.00,
+    },
+    {
+      item: "Watch",
+      price: 30.00,
+      tax: ""
+    },
+    {
+      item: "Water Glass Set",
+      price: 35.00,
+      tax: ""
+    }
+  ],
+  subtotal: 156,
+  total: 156,
+  order_number: 1234222,
+  payment_id: "aafa2fa3f5af",
+  order_id: "order-54464",
+  header: {
+    company_name: "Prakarsh 2021",
+    company_logo: "prakarsh.png",
+    company_address: "Nice Invoice. 123 William Street 1th Floor New York, NY 123456"
+  },
+  footer: {
+    text: "Copyright ©\n Note"
+  },
+  currency_symbol: "₹",
+  date: {
+    billing_date: "08 August 2020",
+
+  }
+};
+
+
+async function main(pdfData) {
+  // Generate test SMTP service account from ethereal.email
+  // Only needed if you don't have a real mail account for testing
+
+
+  // create reusable transporter object using the default SMTP transport
+  let transporter = nodemailer.createTransport({
+    name: "gmail.com",
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false, // true for 465, false for other ports
+    auth: {
+      user: 'jaypatel19124@gmail.com', // generated ethereal user
+      pass: 'vjzmiggrvmxwnvko', // generated ethereal password
+    },
+  });
+
+  // send mail with defined transport object
+  let info = await transporter.sendMail({
+    from: '"Jay Patel" <jay.19beceg046@gmail.com>', // sender address
+    to: "jay@mehdev.club, jaypatel19124@gmail.com", // list of receivers
+    subject: "Hello ✔", // Subject line
+    text: "Here is your receipt", // plain text body
+
+
+    attachments: [
+      {
+        filename: "Invoice.pdf",
+        path: pdfData,
+      }
+    ]
+
+  });
+
+
+  console.log("Message sent: %s", info.messageId);
+  // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+
+  // Preview only available when sending through an Ethereal account
+  console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+  // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+}
+
+niceInvoice(invoiceDetail, main);
+// main().catch(console.error);
---
+++ nice-invoice/app.js
@@ -0,0 +1,225 @@
+const fs = require("fs");
+const PDFDocument = require("pdfkit");
+const stream = require("./stream");
+
+let niceInvoice = (invoice, func) => {
+  let doc = new PDFDocument({ size: "A4", margin: 40, })
+  let writeStream = new stream.WritableBufferStream();
+  doc.pipe(writeStream);
+  doc.image(invoice.niceInvoice.prakarsh_logo, 50, 200, { widht: 500, height: 500 })
+
+  header(doc, invoice);
+  customerInformation(doc, invoice);
+  invoiceTable(doc, invoice);
+  footer(doc, invoice);
+  let buffers = [];
+  let pdfData;
+  doc.on('data', buffers.push.bind(buffers));
+  
+  // doc.on('end', () => {
+
+  //   pdfData = Buffer.concat(buffers);
+  //   func(pdfData).catch(console.error);
+    
+  //   // ... now send pdfData as attachment ...
+
+  // });
+
+ 
+  
+  //doc.pipe(fs.createWriteStream(path));
+
+  writeStream.on('finish', () => {
+    // console log pdf as bas64 string
+    console.log(writeStream.toBuffer().toString('base64'));
+    func(writeStream.toBuffer()).catch(console.error);
+  });
+  doc.end();
+
+}
+
+let header = (doc, invoice) => {
+
+  if (fs.existsSync(invoice.header.company_logo)) {
+    doc.image(invoice.header.company_logo, 50, 45, { width: 50 })
+      .fontSize(25)
+      .text(invoice.header.company_name, 110, 63)
+      .font("Helvetica-Bold")
+      .moveDown();
+  } else {
+    doc.fontSize(20)
+      .text(invoice.header.company_name, 50, 45)
+      .moveDown()
+  }
+
+  if (invoice.header.company_address.length !== 0) {
+    companyAddress(doc, invoice.header.company_address);
+  }
+
+}
+
+let customerInformation = (doc, invoice) => {
+  doc
+    .fillColor("#444444")
+    .fontSize(20)
+    .text("Receipt", 50, 160);
+
+  generateHr(doc, 185);
+
+  const customerInformationTop = 200;
+
+  doc.fontSize(10)
+    .text("Invoice Number:", 50, customerInformationTop)
+    .font("Helvetica-Bold")
+    .text(invoice.order_number, 150, customerInformationTop)
+    .font("Helvetica")
+    .text("Payment Id:", 50, customerInformationTop + 15)
+    .font("Helvetica-Bold")
+    .text(invoice.payment_id, 150, customerInformationTop + 15)
+    .font("Helvetica")
+    .text("Billing Date:", 50, customerInformationTop + 30)
+    .text(invoice.date.billing_date, 150, customerInformationTop + 30)
+    .text("Order Id:", 50, customerInformationTop + 45)
+    .text(invoice.order_id, 150, customerInformationTop + 45)
+    .font("Helvetica-Bold")
+    .text(invoice.shipping.name, 350, customerInformationTop)
+    .font("Helvetica")
+    .text(invoice.shipping.college, 350, customerInformationTop + 15)
+    .text(invoice.shipping.mobile, 350, customerInformationTop + 45)
+    .text(invoice.shipping.email, 350, customerInformationTop + 60)
+    .moveDown();
+
+  generateHr(doc, 280);
+}
+
+let invoiceTable = (doc, invoice) => {
+  let i;
+  const invoiceTableTop = 400;
+  const currencySymbol = invoice.currency_symbol;
+
+  doc.font("Helvetica-Bold");
+  tableRow(
+    doc,
+    invoiceTableTop,
+    "Item",
+    "",
+    "Unit Cost",
+    "",
+    "Total",
+    ""
+  );
+  generateHr(doc, invoiceTableTop + 20);
+  doc.font("Helvetica");
+
+  for (i = 0; i < invoice.items.length; i++) {
+    const item = invoice.items[i];
+    const position = invoiceTableTop + (i + 1) * 30;
+    tableRow(
+      doc,
+      position,
+      item.item,
+      item.description,
+      formatCurrency(item.price, currencySymbol),
+      item.quantity,
+      formatCurrency(applyTaxIfAvailable(item.price, item.quantity), currencySymbol),
+
+    );
+
+    generateHr(doc, position + 20);
+  }
+
+  const subtotalPosition = invoiceTableTop + (i + 1) * 30;
+  doc.font("Helvetica-Bold");
+  totalTable(
+    doc,
+    subtotalPosition,
+    "Subtotal",
+    formatCurrency(invoice.total, currencySymbol)
+  );
+
+  const paidToDatePosition = subtotalPosition + 20;
+  doc.font("Helvetica-Bold");
+  totalTable(
+    doc,
+    paidToDatePosition,
+    "Total",
+    formatCurrency(invoice.total, currencySymbol)
+  );
+}
+
+let footer = (doc, invoice) => {
+  if (invoice.footer.text.length !== 0) {
+    doc.fontSize(10).text(invoice.footer.text, 50, 700, { align: "center", width: 500 });
+  }
+}
+
+let totalTable = (
+  doc,
+  y,
+  name,
+  description
+) => {
+  doc
+    .fontSize(10)
+    .text(name, 400, y, { width: 90, align: "right" })
+    .text(description, 0, y, { align: "right" })
+}
+
+let tableRow = (
+  doc,
+  y,
+  item,
+  description,
+  unitCost,
+  quantity,
+  lineTotal,
+  tax
+) => {
+  doc
+    .fontSize(10)
+    .text(item, 50, y)
+    .text(description, 130, y)
+    .text(unitCost, 280, y, { width: 90, align: "right" })
+    .text(quantity, 335, y, { width: 90, align: "right" })
+    .text(lineTotal, 400, y, { width: 90, align: "right" })
+    .text(tax, 0, y, { align: "right" });
+}
+
+let generateHr = (doc, y) => {
+  doc
+    .strokeColor("#aaaaaa")
+    .lineWidth(1)
+    .moveTo(50, y)
+    .lineTo(550, y)
+    .stroke();
+}
+
+let formatCurrency = (cents, symbol) => {
+  return symbol + cents.toFixed(2);
+}
+
+
+
+
+let applyTaxIfAvailable = (price, quantity) => {
+
+
+
+  var itemPrice = price;
+
+
+  return itemPrice;
+}
+
+let companyAddress = (doc, address) => {
+  let str = address;
+  let chunks = str.match(/.{0,25}(\s|$)/g);
+  let first = 50;
+  chunks.forEach(function (i, x) {
+    doc.fontSize(10).text(chunks[x], 200, first, { align: "right" });
+    first = +first + 15;
+  });
+}
+
+module.exports = niceInvoice;
+
